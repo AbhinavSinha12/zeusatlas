f5c278b30ef96ed2e2dd67955eecb5d6c1b5f7b6
cole.anagnost <cole.anagnost@f71d0cb2-108b-11df-95c3-6ff5f211243c>
2010-02-22 09:18:00 +0000

    Rhino implemented, if the manifest file messes you up use the config walkthrough in the doc folder to set up your target platform

--- trunk/Zeus/src/cpre492/sdmay1020/plugin/zeus/ZeusRunButtonAction.java
+++ trunk/Zeus/src/cpre492/sdmay1020/plugin/zeus/ZeusRunButtonAction.java
@@ -5,6 +5,10 @@ import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.ContextFactory;
+import org.mozilla.javascript.ImporterTopLevel;
+import org.mozilla.javascript.Scriptable;
 
 public class ZeusRunButtonAction implements IWorkbenchWindowActionDelegate {
 
@@ -30,8 +34,33 @@ public class ZeusRunButtonAction implements IWorkbenchWindowActionDelegate {
 
 	@Override
 	public void run(IAction action) {
-		//TODO: launch Rhino
-		MessageDialog.openInformation(window.getShell(), "Sample", "Hello World!");
+		//TODO: get scriptString from file system or eclipse editor
+		//scriptString: The  script, as a single string
+		String scriptString = "java.lang.System.out.println(\"Hello world! This came from javascript!\")";
+			
+		try {
+      // create a private ContextFactory that uses this plug-in's class loader
+			ContextFactory cf = new ContextFactory();
+			cf.initApplicationClassLoader(Zeus.class.getClassLoader());
+			
+			// create a new context which can use "importClass, importPackage"
+			Context cx = cf.enterContext();
+			cx.setLanguageVersion(Context.VERSION_1_7);
+			Scriptable scope = new ImporterTopLevel(cx);
+
+			// run script 
+			Object result = cx.evaluateString(scope, scriptString, "<cmd>", 1, null);
+			//Don't need to print result at the moment, but could uncomment for debugging purposes
+			//System.out.println(Context.toString(result));
+	  } catch(Exception e) {
+	  		System.out.println(e.toString());
+	  } finally {
+	      Context.exit();
+	  }
+		
+		MessageDialog.openInformation(window.getShell(), "Hello World!", "Sample Script Completed.");
+		
+		
 	}
 
 	@Override
