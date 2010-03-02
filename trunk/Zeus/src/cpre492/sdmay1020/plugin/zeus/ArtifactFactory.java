635aadbb237543ab51b0220bf2989473cced065d
tina.gervais <tina.gervais@f71d0cb2-108b-11df-95c3-6ff5f211243c>
2010-03-02 22:12:28 +0000

    Added showResults(IValue value) method based on Jon's method in the sample Atlas API client.  It prints the input set to the console in the launching instance of Eclipse

--- trunk/Zeus/src/cpre492/sdmay1020/plugin/zeus/ArtifactFactory.java
+++ trunk/Zeus/src/cpre492/sdmay1020/plugin/zeus/ArtifactFactory.java
@@ -78,4 +78,28 @@ public class ArtifactFactory {
 		
 		return retval;
 	}
+	public static void showResult(IValue result) {
+		if (result instanceof IArtifacts) {
+			IArtifacts artifacts = (IArtifacts) result;
+			
+			StringBuilder s = new StringBuilder();
+			
+			for (IArtifact a : artifacts) {
+				s.append(a.getName());
+				s.append("\n");
+			}
+			
+			System.out.print("Atlas Query Result\n\nArtifacts:\n" + s.toString());
+			
+		} else if (result instanceof IVariable) {
+			// not really possible at this time - query language only returns artifacts
+			IVariable v = (IVariable) result;
+			System.out.print("Atlas Query Result\n\nVariable: " + v.getName());
+			
+		} else if (result instanceof IStringValue) {
+			// not really possible at this time - query language only returns artifacts
+			IStringValue s = (IStringValue) result;
+			System.out.print("Atlas Query Result\n\nStringValue: " + s.getValue());
+		}
+	}
 }
