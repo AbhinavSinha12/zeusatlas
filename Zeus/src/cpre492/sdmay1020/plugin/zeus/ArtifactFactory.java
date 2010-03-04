1e0dd3735f1ed0fa648a65419d3b58ed1463eb9d
alexf4 <alexf4@f71d0cb2-108b-11df-95c3-6ff5f211243c>
2010-03-04 19:28:35 +0000

    Finished commenting the ArtifactFactory. Two TODOs left, Just need Tina to add some comments to the class and her function.

--- Zeus/src/cpre492/sdmay1020/plugin/zeus/ArtifactFactory.java
+++ Zeus/src/cpre492/sdmay1020/plugin/zeus/ArtifactFactory.java
@@ -3,83 +3,127 @@ package cpre492.sdmay1020.plugin.zeus;
 import com.ensoftcorp.plugin.atlas.query.api.QueryFactory;
 import com.ensoftcorp.plugin.atlas.query.lang.*;
 
+/**
+ * TODO : Tina to explain why we need this class
+ * 
+ * 
+ * @author Alex Kharbush
+ * @author Tina Gervais
+ *
+ */
 public class ArtifactFactory {
 	
 	/**
-	 * Needs to be able to return 
-	 * @param input
-	 * @return
+	 * This static function will take a string argument and return an IFunctionArtifact
+	 * 
+	 * @param input - The string object that will be made into an IArtifact
+	 * @return The IFunctionArtifact that was created with qf.createFunctionArtifact(String input)
+	 * 
+	 * @author Alex Kharbush
 	 */
 	public static IFunctionArtifact createFunction(String input)
 	{
-		
+		//Create a new instance of the QueryFactory
 		QueryFactory qf = QueryFactory.instance;
 		
+		//Return the IFunctionArtifact
 		return qf.createFunctionArtifact(input);
-		
-		
 	}
 	
+	/**
+	 * This static function will take a string argument and return an ITypeArtifact
+	 * 
+	 * @param input - The string object that will be made into an IArtifact
+	 * @return The ITypeArtifact that was created with qf.createTypeArtifact(String input)
+	 * 
+	 * @author Alex Kharbush
+	 */
 	public static ITypeArtifact createType(String input)
 	{
+		//Create a new instance of the QueryFactory
 		QueryFactory qf = QueryFactory.instance;
 		
+		//Return the ITypeArtifact
 		return qf.createTypeArtifact(input);
 	}
 	
+	/**
+	 * This static function will take a string argument and return an IVariableArtifact
+	 * 
+	 * @param input - The string object that will be made into an IArtifact
+	 * @return The IVariableArtifact that was created with qf.createVariableArtifact(String input)
+	 * 
+	 * @author Alex Kharbush
+	 */
 	public static IVariableArtifact createVarible(String input)
 	{
-		
-		
+		//Create a new instance of the QueryFactory
 		QueryFactory qf = QueryFactory.instance;
 		
+		//Return the IVaribleArtifact
 		return qf.createVariableArtifact(input);
 	}
 	
 	/**
-	 * Creates an Atlas Macro Artifact to use as input in script execution
+	 * This static function will take a string argument and return an IMacroArtifact
+	 * 
+	 * @param input - The string object that will be made into an IMacroArtifact
+	 * @return The IFunctionArtifact that was created with qf.createMacroArtifact(String input)
 	 * 
 	 * @note We will not be supporting this
-	 * @param input - String representing MACRO name
-	 * @return an Atlas Macro Artifact
+	 * 
+	 * @author Alex Kharbush
 	 */
 	public static IMacroArtifact createMacro(String input)
 	{
-		
+		//Create a new instance of the QueryFactory
 		QueryFactory qf = QueryFactory.instance;
 		
+		//Return the IMacroArtifact
 		return qf.createMacroArtifact(input);
 	}
 
 	
 	/**
+	 * This static function will take a string argument and return an IFunctionArtifact
 	 * 
-	 * @return
+	 * @param input - The string object that will be made into an IArtifact
+	 * @return The IFunctionArtifact that was created with qf.createFunctionArtifact(String input)
+	 * 
+	 * @author Alex Kharbush
 	 */
 	public static IArtifacts createArtifacts()
 	{
+		//Create a new instance of the QueryFactory
 		QueryFactory qf = QueryFactory.instance;
 		
-		IArtifacts retval = qf.createArtifacts();
-		
-		return retval;
+		//Return the IArtifacts
+		return qf.createArtifacts();
 	}
 	
 	/**
+	 * This static function will take a string argument and return an IStringValue
 	 * 
-	 * @param input
-	 * @return
+	 * @param input - The string object that will be made into an IStringValue
+	 * @return The IFunctionArtifact that was created with qf.createStringValue(String input)
+	 * 
+	 * @author Alex Kharbush
 	 */
 	public static IStringValue createString(String input)
 	{
+		//Create a new instance of the QueryFactory
 		QueryFactory qf = QueryFactory.instance;
 		
-		IStringValue retval = qf.createStringValue(input);
-		
-		return retval;
+		//Return the IStringValue
+		return qf.createStringValue(input);
 	}
 	
 	
+	/**
+	 * TODO : Tina to comment this function
+	 * @author Tina Gervais
+	 * 
+	 */
 	public static void showResult(IValue result) {
 		if (result instanceof IArtifacts) {
 			IArtifacts artifacts = (IArtifacts) result;
