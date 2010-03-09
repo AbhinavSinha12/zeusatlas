c93a48489b4f3439c958e71d3b01d13c5af77962
tina.gervais <tina.gervais@f71d0cb2-108b-11df-95c3-6ff5f211243c>
2010-03-09 21:31:04 +0000

    Added Test Classes Updated comments in Zeus

---
+++ trunk/Zeus/test/cpre492/sdmay1020/plugin/zeus/AtlasQueryAdapterTest.java
@@ -0,0 +1,129 @@
+package cpre492.sdmay1020.plugin.zeus;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AtlasQueryAdapterTest {
+
+	@Test
+	public void testRunMinusQuery() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRunAndQuery() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRunOrQuery() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRunDefQuery() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRunArgumentCastQuery() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRunArgumentQuery() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRunArtifactsQuery() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRunCalledByQuery() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRunCallGraphQuery() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRunCallQuery() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRunCastQuery() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRunFunctionDeclareQuery() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRunFunctionsRegExQuery() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRunLeavesQuery() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRunReadByQuery() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRunReadQuery() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRunReferencedByQuery() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRunReferenceQuery() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRunReverseCallGraphQuery() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRunRootsQuery() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRunTypesQuery() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRunVariablesQuery() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRunWriteQuery() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRunWrittenByQuery() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ trunk/Zeus/test/cpre492/sdmay1020/plugin/zeus/ZeusRunButtonActionTest.java
@@ -0,0 +1,53 @@
+package cpre492.sdmay1020.plugin.zeus;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ZeusRunButtonActionTest {
+
+	ZeusRunButtonAction z  = null;
+	
+	@Before 
+	public void setUp() { 
+		z = new ZeusRunButtonAction();
+	}
+	
+	@After 
+	public void tearDown() { 
+		z = null;
+	}
+	
+	@Test
+	public void testZeusRunButtonAction() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetScript() {
+		fail("Not yet implemented");		
+	}
+
+	@Test
+	public void testDispose() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRun() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSelectionChanged() {
+		fail("Not yet implemented");
+	}
+
+}
