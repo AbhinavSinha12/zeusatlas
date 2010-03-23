a1f584130a83738e97088690340cc1460c9ba4a4
tina.gervais <tina.gervais@f71d0cb2-108b-11df-95c3-6ff5f211243c>
2010-03-23 21:14:31 +0000

    Pulled displaying query results out of ArtifactFactory and moved to the new OutputResults class.
    Also added functions to show a graph and create an XML file

--- trunk/Zeus/src/cpre492/sdmay1020/plugin/zeus/ArtifactFactory.java
+++ trunk/Zeus/src/cpre492/sdmay1020/plugin/zeus/ArtifactFactory.java
@@ -118,42 +118,4 @@ public class ArtifactFactory {
 		return qf.createStringValue(input);
 	}
 	
-	
-	// TODO switch method to print to a file instead of the console
-	
-	/**
-	 * showResult prints the type and name of the input IValue (or IValue array) in an
-	 * easy to read format
-	 * 
-	 * @param header - The header string that is printed before the result values
-	 * @param result - The query results to print (can be a single IValue or an array of IValue objects)
-	 * @author Tina Gervais
-	 */
-	public static void showResult(String header, IValue... result) {
-		System.out.println(header+"\n");
-		for(IValue r : result){
-			if (r instanceof IArtifacts) {
-				IArtifacts artifacts = (IArtifacts) r;
-				
-				StringBuilder s = new StringBuilder();
-				
-				for (IArtifact a : artifacts) {
-					s.append(a.getName());
-					s.append("\n  ");
-				}		
-				System.out.println("Artifacts:\n  " + s.toString());
-				
-			} else if (r instanceof IVariable) {
-				// not really possible at this time - query language only returns artifacts
-				IVariable v = (IVariable) r;
-				System.out.println("Variable: " + v.getName());
-				
-			} else if (r instanceof IStringValue) {
-				// not really possible at this time - query language only returns artifacts
-				IStringValue s = (IStringValue) r;
-				System.out.println("StringValue: " + s.getValue());
-			}
-		}
-		System.out.println("\n");
-	}
 }
