a9577ada563dc4d8283216039fd7c25092058d65
alexf4 <alexf4@f71d0cb2-108b-11df-95c3-6ff5f211243c>
2010-03-23 18:44:53 +0000

    Added the Graph test, not sure if it will work, but the syntax looks correct

--- trunk/Zeus/test/cpre492/sdmay1020/plugin/zeus/ZeusTest.java
+++ trunk/Zeus/test/cpre492/sdmay1020/plugin/zeus/ZeusTest.java
@@ -4,6 +4,16 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.ensoftcorp.plugin.atlas.query.api.QueryFactory;
+import com.ensoftcorp.plugin.atlas.query.lang.IArtifacts;
+import com.ensoftcorp.plugin.atlas.query.lang.IQueryFunction;
+import com.ensoftcorp.plugin.atlas.query.lang.IQueryFunctionSymbolTable;
+import com.ensoftcorp.plugin.atlas.query.lang.IQueryState;
+import com.ensoftcorp.plugin.atlas.query.lang.IStringValue;
+import com.ensoftcorp.plugin.atlas.query.lang.IValue;
+import com.ensoftcorp.plugin.atlas.query.lang.IQueryFunctionSymbolTable.FUNCTION;
+import com.ensoftcorp.plugin.atlas.ui.api.GraphUI;
+
 public class ZeusTest {
 
 	@Test
@@ -29,6 +39,41 @@ public class ZeusTest {
 	@Test
 	public void testGetImageDescriptor() {
 		fail("Not yet implemented");
+	}
+	
+	/**
+	 * This test will check to make sure we can send the results to graph gui.
+	 */
+	@Test
+	public void testDrawGraph()
+	{
+		
+		QueryFactory qf = QueryFactory.instance;
+		IQueryState queryState = qf.createQueryState();
+		IQueryFunctionSymbolTable qfst = qf.createQueryFunctionSymbolTable();
+		
+		IStringValue sv = qf.createStringValue("net.h");
+		IQueryFunction def = qfst.lookupSymbol(FUNCTION.DEF);
+		IQueryFunction ref = qfst.lookupSymbol(FUNCTION.REF);
+		IValue result2 = def.execute(qfst, queryState, new IValue[] {sv});
+		IValue result3 = ref.execute(qfst, queryState, new IValue[] {result2});
+		
+		
+		IArtifacts roots = QueryFactory.instance.createArtifacts();
+		IArtifacts leaves = QueryFactory.instance.createArtifacts();
+		IArtifacts omissions = QueryFactory.instance.createArtifacts();
+		IArtifacts highlights = QueryFactory.instance.createArtifacts();
+		IArtifacts simplify = QueryFactory.instance.createArtifacts();
+		IArtifacts center = QueryFactory.instance.createArtifacts();
+		
+		leaves = (IArtifacts) result3;
+		highlights = (IArtifacts) result3;
+		
+		GraphUI.instance.showGraph("net.h", roots, leaves, omissions, highlights, simplify, center);
+		
+		
+		fail("Not yet implemented");
+	
 	}
 
 }
