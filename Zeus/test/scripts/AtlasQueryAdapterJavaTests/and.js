f22da936d7214d914d46dc5392e2f6d9ca54accc
alexf4 <alexf4@f71d0cb2-108b-11df-95c3-6ff5f211243c>
2010-04-06 01:02:54 +0000

    Created js scripts for all the wrappers. Still need to test them to find results

---
+++ Zeus/test/scripts/AtlasQueryAdapterJavaTests/and.js
@@ -0,0 +1,39 @@
+//Imports
+	
+//var Zeus = JavaImporter(Packages.cpre492.sdmay1020.plugin.zeus.ArtifactFactory, 
+//						Packages.cpre492.sdmay1020.plugin.zeus.AtlasQueryAdapter,
+//                      Packages.cpre492.sdmay1020.plugin.zeus.OutputResults );
+
+with(Zeus){
+
+//Creation of an IFunctionArtifact
+var IFunctionArtifact = ArtifactFactory.createFunction("getbuf");
+var IFunctionArtifact = ArtifactFactory.createFunction("freebuf");
+
+//Create an IArtifacts object
+var IArtifacts = ArtifactFactory.createArtifacts();
+var IArtifacts2 = ArtifactFactory.createArtifacts();
+
+
+//Create an empty IArtifacts object
+//We will use this to pas into showGraph
+var empty = ArtifactFactory.createArtifacts();
+
+//Add the IFunctionArtirfact to the collection IArtifacts
+IArtifacts.add(IFunctionArtifact);
+IArtifacts2.add(IFunctionArtifact);
+
+//create an IArtifactFunction that holds the result of "calledby(getbuf)"
+var Results = AtlasQueryAdapter.and(IArtifacts, IArtifacts2);
+
+//Name of the graph
+var name = "Graph Test";
+
+//Pass the name and Results to the graph
+//NOTE : we pass the name first, then pass the results as the root node,
+//and finally pass the empty IArtifacts set to the showgraph
+OutputResults.showGraph(name, Results, empty,empty, empty,empty,empty);
+
+}
+
+
