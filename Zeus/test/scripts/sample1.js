3c8302db6dd8cbce0e2f467f9f9d604ceba9881e
tina.gervais <tina.gervais@f71d0cb2-108b-11df-95c3-6ff5f211243c>
2010-04-24 22:46:10 +0000

    Updated import & alias code to fix runtime errors

--- Zeus/test/scripts/sample1.js
+++ Zeus/test/scripts/sample1.js
@@ -1,4 +1,4 @@
 var set = af.createArtifacts(); 
 set.add(af.createFunction("dswrite")); 
-var r1 = aq.runCalledByQuery(set);
-af.showResult("Called By dswrite", r1);
+var r1 = aq.calledby(set);
+or.toTextFile("Called By dswrite", r1);
